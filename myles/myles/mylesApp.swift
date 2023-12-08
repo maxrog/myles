1a47aaf0bb91eefd350bc6a0868011cd99ffd4ea
Max Rogers <max.rogers@yinzcam.com>
2023-12-08 14:37:05 -0500

    Add Xcode Project Skeleton

---
+++ myles/myles/mylesApp.swift
@@ -0,0 +1,17 @@
+//
+//  mylesApp.swift
+//  myles
+//
+//  Created by Max Rogers on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct mylesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
