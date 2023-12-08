11c73b9b66c5d228f9db1687feea2cb1b528ffaa
Max Rogers <max.rogers@yinzcam.com>
2023-12-08 15:07:43 -0500

    remove unused uikit extension

--- myles/myles/Utilities/Extensions/UIKitExtensions.swift
+++ myles/myles/Utilities/Extensions/UIKitExtensions.swift
@@ -8,14 +8,3 @@
 import Foundation
 
 import UIKit
-
-// MARK: UIApplication
-
-extension UIApplication {
-    
-    /// The main scene used throughout the app
-    public static var mainScene: UIScene? {
-        Self.shared.connectedScenes.first
-    }
-    
-}
