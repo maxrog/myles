8b9e45e2639a09a876c9fe59b6725debf751f0b8
Max Rogers <max.rogers@yinzcam.com>
2023-12-20 08:40:35 -0500

    default accent color based on system dark vs light mode

--- myles/myles/Services/ThemeManager.swift
+++ myles/myles/Services/ThemeManager.swift
@@ -22,7 +22,7 @@ class ThemeManager: ObservableObject {
     let userDefaults = UserDefaults.standard
     
     /// Default
-    static let defaultAccentColor = Color(uiColor: UIColor(named: "CosmicLatte") ?? .white)
+    static let defaultAccentColor = (UIScreen.main.traitCollection.userInterfaceStyle == .dark) ? Color(uiColor: UIColor(named: "CosmicLatte") ?? .white) : Color(uiColor: UIColor(named: "mylesMedium") ?? .white)
     
     init() {
         setupTheme()
